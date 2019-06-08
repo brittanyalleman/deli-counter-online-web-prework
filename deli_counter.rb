2c95b21497c82de494bfcb12450a5b0a2ac51f23
Brittany Alleman <brittany.alleman1@gmail.com>
2019-06-08 22:53:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,8 @@
 def line(array)
  if array.count == 0
    puts "The line is currently empty."
- end
  else
    array.each_with_index do |name, i|
      "The line is currently: (string + i + name exit)"
  end
+end
