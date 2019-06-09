0fca472264a2f84898cf4e09afda844eb1be08d9
Brittany Alleman <brittany.alleman1@gmail.com>
2019-06-09 00:24:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,11 +5,11 @@ def line(array)
     empty_line="The line is currently:"
    array.each_with_index do |name,i|
     empty_line +=" #{i+1}. #{name}"
- else
+   else
    empty_line="The line is currently:"
   array.each_with_index do |name,i|
    empty_line +=" #{i+1}. #{name}"
-
+   end
   end
   puts empty_line
  end
