4ab4d10dc371c5d8d29f6262729b48c6cee99dd6
Brittany Alleman <brittany.alleman1@gmail.com>
2019-06-09 00:51:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ end
 
 def take_a_number(katz_deli, i )
  if katz_deli.count == 0
-   puts ("Welcome, #{katz_deli}. You are number #{i=1} in line.")
+   puts ("Welcome, #{array}. You are number #{i=1} in line.")
  #else
    #take_a_number= ("Welcome, #{name}. You are number #{i=1} in line. ")
  end
