1daf24150adc37a17a4e8feb8ffa8809c1693c73
Brittany Alleman <brittany.alleman1@gmail.com>
2019-06-09 00:38:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,15 @@ def line(array)
     empty_line="The line is currently:"
    array.each_with_index do |name,i|
     empty_line +=" #{i+1}. #{name}"
-
   end
   puts empty_line
+ end
+end
+
+def take_a_number(array)
+ if array.count ==0
+   puts "The line is currently empty."
+ else
+   take_a_number="welcome, #{name}. You are number #{i=1} in line. "
  end
 end
